da6d7002f0f686113a85ddfc9ebd9d6891e2535a
crdavip <crdavip@gmail.com>
2024-07-21 14:25:34 -0500

    Backend de Fichas listo

--- model/fichas.php
+++ model/fichas.php
@@ -1,24 +1,67 @@
 <?php
 
 require_once ('../model/db.php');
+require_once ('../controller/funciones.php');
 
 class Fichas extends ConnPDO
 {
 
+  private $functions;
+
   public function __construct()
   {
     parent::__construct();
+    $this->functions = new Funciones();
   }
 
   function getDataSheets()
   {
-    $sql = "SELECT f.idFicha, f.ficha, f.detalle AS curso, f.aprendices, c.siglas AS centro
+    $sql = "SELECT f.idFicha, f.ficha, f.detalle AS curso, f.aprendices, f.idCentro, c.siglas AS centro
             FROM ficha AS f
             INNER JOIN centro AS c ON c.idCentro = f.idCentro ";
     $stmt = $this->getConn()->prepare($sql);
     $stmt->execute();
     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($row, JSON_UNESCAPED_UNICODE);
+  }
+
+  function createDataSheets($num, $detail, $idCenter)
+  {
+    $sql = "INSERT INTO ficha (ficha, detalle, aprendices, idCentro) VALUES (?, ?, 0, ?)";
+    $stmt = $this->getConn()->prepare($sql);
+    if ($stmt->execute([$num, $detail, $idCenter])) {
+      $icon = $this->functions->getIcon('OK');
+      echo json_encode(['success' => true, 'message' => "$icon ¡Ficha Creada Exitosamente!"]);
+    } else {
+      $icon = $this->functions->getIcon('Err');
+      echo json_encode(['success' => false, 'message' => "$icon Error al crear la ficha"]);
+    }
+  }
+
+  function updateDataSheets($num, $detail, $idCenter, $idDataSheet)
+  {
+    $sql = "UPDATE ficha SET ficha=?, detalle=?, idCentro=? WHERE idFicha=?";
+    $stmt = $this->getConn()->prepare($sql);
+    if ($stmt->execute([$num, $detail, $idCenter, $idDataSheet])) {
+      $icon = $this->functions->getIcon('OK');
+      echo json_encode(['success' => true, 'message' => "$icon ¡Ficha Actualizada Exitosamente!"]);
+    } else {
+      $icon = $this->functions->getIcon('Err');
+      echo json_encode(['success' => false, 'message' => "$icon Error al editar la ficha"]);
+    }
+  }
+
+  function deleteDataSheets($idDataSheet)
+  {
+    $sql = "DELETE FROM ficha WHERE idFicha=?";
+    $stmt = $this->getConn()->prepare($sql);
+    if ($stmt->execute([$idDataSheet])) {
+      $icon = $this->functions->getIcon('OK');
+      echo json_encode(['success' => true, 'message' => "$icon ¡Ficha Eliminada Exitosamente!"]);
+    } else {
+      $icon = $this->functions->getIcon('Err');
+      echo json_encode(['success' => false, 'message' => "$icon Error al eliminar la ficha"]);
+    }
   }
 
 }
--- view/js/fichas.js
+++ view/js/fichas.js
@@ -5,88 +5,143 @@ loadSelectFilters(centrosAPI, "centerSelectFilter", ["siglas"]);
 
 let dataSheets = [];
 const loadDataSheets = async () => {
-    const data = await getData(fichasAPI);
-    dataSheets = data;
-    renderDataSheets(dataSheets);
-}
+  const data = await getData(fichasAPI);
+  dataSheets = data;
+  renderDataSheets(dataSheets);
+};
 
 window.addEventListener("DOMContentLoaded", loadDataSheets);
 
 const updateDataSheets = async () => {
-    await loadDataSheets();
+  await loadDataSheets();
 };
 
 const createDataSheetCard = (dataSheets) => {
-    const fragment = document.createDocumentFragment();
-    dataSheets.forEach((sheet) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const cardTop = document.createElement("div");
-        cardTop.classList.add("cardTop");
-        if (userRolView == 1) {
-            const cardMenu = document.createElement("a");
-            cardMenu.classList.add("cardMenu");
-            cardMenu.innerHTML = '<i class="fa-solid fa-ellipsis"></i>';
-            const cardMenuItems = document.createElement("div");
-            cardMenuItems.classList.add("cardMenuItems");
-            const btnTrainees = document.createElement("a");
-            btnTrainees.innerHTML = '<i class="fa-solid fa-user-graduate"></i>Aprendices';
-            const btnEdit = document.createElement("a");
-            btnEdit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>Editar';
-            const btnDelete = document.createElement("a");
-            btnDelete.innerHTML = '<i class="fa-solid fa-trash"></i>Eliminar';
-            cardMenuItems.appendChild(btnTrainees);
-            cardMenuItems.appendChild(btnEdit);
-            cardMenuItems.appendChild(btnDelete);
-            cardTop.appendChild(cardMenuItems);
-            cardTop.appendChild(cardMenu);
-            dropDown(cardMenu, cardMenuItems);          
-        }
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("cardBody");
-        const cardDataSheetNum = document.createElement("div");
-        cardDataSheetNum.classList.add("cardDataSheetNum");
-        cardDataSheetNum.innerHTML = `<h2>${sheet.ficha}</h2>`;
-        cardBody.appendChild(cardDataSheetNum);
-        const cardBodyTxt = document.createElement("div");
-        cardBodyTxt.classList.add("cardBodyTxt");
-        cardBodyTxt.innerHTML = `<p>${sheet.curso}</p>
+  const fragment = document.createDocumentFragment();
+  dataSheets.forEach((sheet) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardTop = document.createElement("div");
+    cardTop.classList.add("cardTop");
+    if (userRolView == 1) {
+      const cardMenu = document.createElement("a");
+      cardMenu.classList.add("cardMenu");
+      cardMenu.innerHTML = '<i class="fa-solid fa-ellipsis"></i>';
+      const cardMenuItems = document.createElement("div");
+      cardMenuItems.classList.add("cardMenuItems");
+      const btnTrainees = document.createElement("a");
+      btnTrainees.innerHTML =
+        '<i class="fa-solid fa-user-graduate"></i>Aprendices';
+      const btnEdit = document.createElement("a");
+      btnEdit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>Editar';
+      const btnDelete = document.createElement("a");
+      btnDelete.innerHTML = '<i class="fa-solid fa-trash"></i>Eliminar';
+      cardMenuItems.appendChild(btnTrainees);
+      cardMenuItems.appendChild(btnEdit);
+      cardMenuItems.appendChild(btnDelete);
+      cardTop.appendChild(cardMenuItems);
+      cardTop.appendChild(cardMenu);
+      dropDown(cardMenu, cardMenuItems);
+      btnEdit.addEventListener("click", () => {
+        loadDataForm({
+          inputs: [
+            "dataSheetIdEdit",
+            "dataSheetNumEdit",
+            "dataSheetCourseEdit",
+            "dataSheetCenterEdit",
+          ],
+          inputsValue: [sheet. idFicha, sheet.ficha, sheet.curso, sheet.idCentro],
+          modal: "dataSheetEdit",
+        });
+      });
+      btnDelete.addEventListener("click", () => {
+        loadDataForm({
+          inputs: ["dataSheetIdDelete"],
+          inputsValue: [sheet.idFicha],
+          modal: "dataSheetDelete",
+        });
+      });
+    }
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("cardBody");
+    const cardDataSheetNum = document.createElement("div");
+    cardDataSheetNum.classList.add("cardDataSheetNum");
+    cardDataSheetNum.innerHTML = `<h2>${sheet.ficha}</h2>`;
+    cardBody.appendChild(cardDataSheetNum);
+    const cardBodyTxt = document.createElement("div");
+    cardBodyTxt.classList.add("cardBodyTxt");
+    cardBodyTxt.innerHTML = `<p>${sheet.curso}</p>
                                 <h3>${sheet.centro}</h3>
-                                <span>${sheet.aprendices} ${sheet.aprendices == 1 ? "Aprendiz" : "Aprendices"}</span>`;
-        cardBody.appendChild(cardBodyTxt);
-        card.appendChild(cardTop);
-        card.appendChild(cardBody);
-        fragment.appendChild(card);
-    });
-    return fragment;
+                                <span>${sheet.aprendices} ${
+      sheet.aprendices == 1 ? "Aprendiz" : "Aprendices"
+    }</span>`;
+    cardBody.appendChild(cardBodyTxt);
+    card.appendChild(cardTop);
+    card.appendChild(cardBody);
+    fragment.appendChild(card);
+  });
+  return fragment;
 };
 
 const row = document.querySelector(".row");
 const renderDataSheets = (data) => {
-    if (data.length > 0) {
-        const cards = createDataSheetCard(data);
-        row.innerHTML = "";
-        row.appendChild(cards);
-      } else {
-        row.innerHTML = "No hay resultados para mostrar.";
-      }
-}
+  if (data.length > 0) {
+    const cards = createDataSheetCard(data);
+    row.innerHTML = "";
+    row.appendChild(cards);
+  } else {
+    row.innerHTML = "No hay resultados para mostrar.";
+  }
+};
 
 const filterDataSheets = () => {
-    const center = centerSelectFilter.value;
-    const number = numberInputFilter.value;
-    let newDataSheets = dataSheets;
-    if (center !== "all") {
-        newDataSheets = newDataSheets.filter((dataSheet) => dataSheet.centro == center);
-    }
-    if (number !== "") {
-        newDataSheets = newDataSheets.filter((dataSheet) =>
-        `${dataSheet.ficha}`.includes(`${number}`)
-      );
-    }
-    renderDataSheets(newDataSheets);
-  };
-  centerSelectFilter.addEventListener("change", filterDataSheets);
-  numberInputFilter.addEventListener("keyup", filterDataSheets);
+  const center = centerSelectFilter.value;
+  const number = numberInputFilter.value;
+  let newDataSheets = dataSheets;
+  if (center !== "all") {
+    newDataSheets = newDataSheets.filter(
+      (dataSheet) => dataSheet.centro == center
+    );
+  }
+  if (number !== "") {
+    newDataSheets = newDataSheets.filter((dataSheet) =>
+      `${dataSheet.ficha}`.includes(`${number}`)
+    );
+  }
+  renderDataSheets(newDataSheets);
+};
+centerSelectFilter.addEventListener("change", filterDataSheets);
+numberInputFilter.addEventListener("keyup", filterDataSheets);
 
 loadSelectFilters(centrosAPI, "dataSheetCenter", ["idCentro", "detalle"]);
+loadSelectFilters(centrosAPI, "dataSheetCenterEdit", ["idCentro", "detalle"]);
+
+sendForm(
+  "dataSheetCreateForm",
+  fichasAPI,
+  "POST",
+  "messageCreate",
+  updateDataSheets,
+  "dataSheetCreate",
+  1500
+);
+
+sendForm(
+  "dataSheetEditForm",
+  fichasAPI,
+  "PUT",
+  "messageEdit",
+  updateDataSheets,
+  "dataSheetEdit",
+  1500
+);
+
+sendForm(
+  "dataSheetDeleteForm",
+  fichasAPI,
+  "DELETE",
+  "messageDelete",
+  updateDataSheets,
+  "dataSheetDelete",
+  1500
+);
